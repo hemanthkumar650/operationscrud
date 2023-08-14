b69962f13b4fbc1ade691e3c7a579e2ab0663bd0
Hemanth Kumar V S <hemanth.kumarvs.intern@lightmetrics.co>
2023-08-14 17:18:23 +0530

    added schema-only without data

--- run_server.sh
+++ run_server.sh
@@ -5,25 +5,25 @@ PG_USER="postgres"
 PG_DB="postgres"
 
 
-NEW_BACKUP_FILENAME="backup_$(date +%Y%m%d%H%M%S).sql"
+NEW_BACKUP_FILENAME="schema_backup_$(date +%Y%m%d%H%M%S).sql"
 
-pg_dump -h "$PG_HOST" -U "$PG_USER" -d "$PG_DB" -f "$BACKUP_DIR/$NEW_BACKUP_FILENAME"
+pg_dump -h "$PG_HOST" -U "$PG_USER" -d "$PG_DB" --schema-only -f "$BACKUP_DIR/$NEW_BACKUP_FILENAME"
 
-if [ -f "$BACKUP_DIR/previous_backup.sql" ]; then
-    if ! cmp -s "$BACKUP_DIR/$NEW_BACKUP_FILENAME" "$BACKUP_DIR/previous_backup.sql"; then
+if [ -f "$BACKUP_DIR/previous_schema_backup.sql" ]; then
+    if ! cmp -s "$BACKUP_DIR/$NEW_BACKUP_FILENAME" "$BACKUP_DIR/previous_schema_backup.sql"; then
     
         cp "$BACKUP_DIR/$NEW_BACKUP_FILENAME" "$REPO_DIR/"
 
         cd "$REPO_DIR"
         git add "$NEW_BACKUP_FILENAME"
-        git commit -m "Update database backup"
+        git commit -m "Update schema backup"
         git push origin master
 
-        cp "$BACKUP_DIR/$NEW_BACKUP_FILENAME" "$BACKUP_DIR/previous_backup.sql"
+        cp "$BACKUP_DIR/$NEW_BACKUP_FILENAME" "$BACKUP_DIR/previous_schema_backup.sql"
     else
         echo "No difference"
     fi
 else
-    echo "No previous backup found. Saving the new backup as the previous backup."
-    cp "$BACKUP_DIR/$NEW_BACKUP_FILENAME" "$BACKUP_DIR/previous_backup.sql"
+    echo "No previous schema backup found. Saving the new schema backup as the previous backup."
+    cp "$BACKUP_DIR/$NEW_BACKUP_FILENAME" "$BACKUP_DIR/previous_schema_backup.sql"
 fi
